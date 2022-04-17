abbfb300ec17608f3fb91ee6d98cc4438abaa600
Ekremteke <ekremteke.uk@gmail.com>
2022-04-17 15:32:07 +0100

    Some of the project description may be Turkish

--- src/main/java/JavaGitHubPractice.java
+++ src/main/java/JavaGitHubPractice.java
@@ -2,6 +2,7 @@ public class JavaGitHubPractice {
     public static void main(String[] args) {
 
         System.out.println("Welcome to my project");
+        System.out.println("Some of the project may Turkish");
 
     }
 }
